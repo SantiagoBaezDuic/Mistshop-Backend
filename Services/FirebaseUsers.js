b1cabfb7f751aadfba1646383ef683ca32bcacae
Santiago Báez Duic <santib1598@hotmail.com>
2022-09-04 23:12:15 -0300

    Added users service

---
+++ Services/FirebaseUsers.js
@@ -0,0 +1,9 @@
+import FBContainer from "../Containers/FirebaseContainer.js";
+
+class UsersFB extends FBContainer {
+    constructor(){
+        super("Users")
+    }
+}
+
+export default UsersFB;
--- Services/index.js
+++ Services/index.js
@@ -3,15 +3,18 @@ dotenv.config();
 
 let ProductService;
 let CartService;
+let UsersService;
 
 switch(process.env.DATABASE.toUpperCase() || "FIREBASE"){
     case "FIREBASE":
         const {default: FirebaseProduct} = await import("./FirebaseProducts.js");
         const {default: FirebaseCart} = await import("./FirebaseCarts.js");
+        const {default: FirebaseUsers} = await import("./FirebaseUsers.js");
 
         ProductService = new FirebaseProduct;
         CartService = new FirebaseCart;
+        UsersService = new FirebaseUsers;
         break;
 }
 
-export {ProductService, CartService};
+export {ProductService, CartService, UsersService};
